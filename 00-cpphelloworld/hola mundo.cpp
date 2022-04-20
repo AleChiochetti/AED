b2449d717f635b0a63bec13d3004dff74e8b8ad3
Alejo Chiochetti <103226480+AleChiochetti@users.noreply.github.com>
2022-04-19 21:31:12 -0300

    Add files via upload

---
+++ 00-cpphelloworld/hola mundo.cpp
@@ -0,0 +1,16 @@
+
+// Header Comments
+
+// hello.cpp
+// Trabajo #0 para AED
+// Chiochetti Alejo
+// 20220419
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hola mundo :D\n";
+    return 0;
+}
